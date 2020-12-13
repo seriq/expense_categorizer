bca58e50e4f90ca115ce8eaa0711cc81a202b628
Eric Schlarmann <e.schlarmann@posteo.de>
2020-12-13 13:04:16 +0100

    Further improve error handling

--- src/categorizer.rs
+++ src/categorizer.rs
@@ -1,13 +1,20 @@
 use crate::model::*;
 
 pub fn categorize_booking(booking: Booking, rules: &Vec<CategoryWithRule>) -> CategorizedBooking {
-    for rule in rules {
-        if rule.check(&booking) {
-            return rule.apply(booking);
-        }
-    }
-    CategorizedBooking {
-        booking,
-        category: "Uncategorized".into(),
+    let applicable_rules: Vec<&CategoryWithRule> = rules
+        .into_iter()
+        .filter(|rule| rule.check(&booking))
+        .collect();
+
+    match applicable_rules.len() {
+        1 => applicable_rules.get(0).unwrap().apply(booking),
+        0 => CategorizedBooking {
+            category: "Uncategorized".into(),
+            booking,
+        },
+        _ => CategorizedBooking {
+            category: "Multiple Matches".into(),
+            booking,
+        },
     }
 }
